7925dd83e1c638ed9b5e6feedaf26bb4793834ba
Akos M <akos.mezner@gmail.com>
2017-11-12 23:32:09 +0100

    Updating Entities in EjB, fixing Controllers in WAR

--- beer_stock.sql
+++ beer_stock.sql
@@ -4,12 +4,20 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+create table user (
+		`ID` INT NOT NULL AUTO_INCREMENT,
+		`name` VARCHAR(20),
+		`role` VARCHAR(20),
+		`password` VARCHAR(20),
+		PRIMARY KEY (ID)
+);
+
 create table stock (
 		`ID` INT NOT NULL AUTO_INCREMENT,
 		`name` VARCHAR(20),
 		`description` VARCHAR(255),
-		`purchaseprice` DOUBLE,
-		`sellingprice` DOUBLE,
+		`purchaseprice` DOUBLE PRECISION,
+		`sellingprice` DOUBLE PRECISION,
 		`onstockquantity` INT,
 		`type` VARCHAR(20),
 		PRIMARY KEY (ID)
@@ -30,13 +38,12 @@ create table invoice(
 		`invoicenumber` int NOT NULL AUTO_INCREMENT,
 		`date` DATETIME,
 		`discount` INT,
-		`customer_id` int,
+		`customer_id` INT,
 		`name` VARCHAR(100),
 		`address` VARCHAR(200),
 		`email` VARCHAR(100),
 		`phone` VARCHAR(20),
 		`loyaltycard` BIT(1),
-		`discount` INT,
 		FOREIGN KEY (`customer_id`) REFERENCES `customer`(`ID`),
 		PRIMARY KEY (`invoicenumber`)
 );
@@ -44,12 +51,12 @@ create table invoice(
 create table invoicedproducts (
 	`invoicenumber` INT,
 	`stockid` INT,
-	'name' VARCHAR(20),
-	'type' VARCHAR(20),
-	'purchaseprice' DOUBLE;
-	`soldprice` DOUBLE,
+	`name` VARCHAR(20),
+	`type` VARCHAR(20),
+	`purchaseprice` DOUBLE PRECISION,
+	`soldprice` DOUBLE PRECISION,
 	`soldquantity` INT,
-	'soldsubtotal' DOUBLE,
+	`soldsubtotal` DOUBLE PRECISION,
 	FOREIGN KEY (`stockid`) REFERENCES `stock`(`ID`),
 	FOREIGN KEY (`invoicenumber`) REFERENCES `invoice`(`invoicenumber`)
 );
